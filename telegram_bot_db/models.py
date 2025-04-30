b4d80733beaa0a067b6ee3fd10f1a4c13375ffeb
RWTLoneWolf <rwtlonewolf@gmail.com>
2025-04-30 23:36:33 +0500

    Update Data Base

--- telegram_bot_db/models.py
+++ telegram_bot_db/models.py
@@ -14,3 +14,16 @@ class MessagesModel(models.Model):
     class Meta:
         verbose_name = "Message"
         verbose_name_plural = "Messages"
+
+class PendingMessagesModel(models.Model):
+    lmid = models.CharField(max_length=255, db_index=True, verbose_name='Pending Message ID', null=True, blank=True)
+    inline_message_id = models.CharField(max_length=255, verbose_name='Inline Message ID')
+    message = models.CharField(max_length=255, verbose_name='Message')
+    timestamp = models.DateTimeField(default=datetime.datetime(1970, 1, 1, 5, 0, 0), verbose_name="Time:")
+
+    def __str__(self):
+        return self.user
+
+    class Meta:
+        verbose_name = "Pending Message"
+        verbose_name_plural = "Pending Messages"
